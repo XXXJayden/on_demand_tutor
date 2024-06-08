642c044c4ec30d39ae302d9b00773460c3a7e704
Nguyen Vi Tien <Nguyevitien2003@gmail.com>
2024-06-08 20:06:07 +0700

    [Base] Achievement, Feedback, Tutor

---
+++ On_Demand_Tutor_System/Services/TutorServices/ITutorService.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.TutorServices
+{
+    public interface ITutorService
+    {
+        List<Tutor> GetAllTutor();
+        void SaveTutor(Tutor tutor);
+        void UpdateTutor(Tutor tutor);
+        void DeleteTutor(short tutorId);
+        Tutor GetTutorById(short tutorId);
+    }
+}
