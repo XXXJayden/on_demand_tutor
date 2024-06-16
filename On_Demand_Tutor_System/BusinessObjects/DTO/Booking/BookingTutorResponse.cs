54d6e1efe68f71f713e3cd70b8c1734e0a0fcab2
diucnguyen <ducnxse171688@fpt.edu.vn>
2024-06-16 23:07:16 +0700

    [Feat] ViewStudentRegister

---
+++ On_Demand_Tutor_System/BusinessObjects/DTO/Booking/BookingTutorResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObjects.DTO.Booking
+{
+    public class BookingTutorResponse
+    {
+        public int Id { get; set; }
+        [Display(Name = "Student Name")]
+        public string StudentName { get; set; }
+
+        public int TutorId { get; set; }
+
+        [Display(Name = "Service Name")]
+        public string ServiceName { get; set; }
+
+        public string Status { get; set; } = null!;
+
+        public DateOnly? DateStart { get; set; }
+
+        public DateOnly? DateEnd { get; set; }
+
+    }
+}
