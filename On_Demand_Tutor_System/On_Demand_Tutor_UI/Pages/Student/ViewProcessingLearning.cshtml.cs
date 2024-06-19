d51fec61f8e269ac187b72528d4334716391cc00
Nguyen Vi Tien <Nguyevitien2003@gmail.com>
2024-06-19 22:44:42 +0700

    [Feat] View ProcessingTeachingofTutor

--- On_Demand_Tutor_System/On_Demand_Tutor_UI/Pages/Student/ViewProcessingLearning.cshtml.cs
+++ On_Demand_Tutor_System/On_Demand_Tutor_UI/Pages/Student/ViewProcessingLearning.cshtml.cs
@@ -4,43 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using BusinessObjects.Models;
 using BusinessObjects.DTO.Booking;
 using Services.BookingService;
 
-namespace On_Demand_Tutor_UI.Pages.Tutor
+namespace On_Demand_Tutor_UI.Pages.Student
 {
-    public class ViewProcessingTeachingModel : PageModel
+    public class ViewProcessingLearningModel : PageModel
     {
         private readonly IBookingService _bookingService;
 
-        public ViewProcessingTeachingModel(IBookingService bookingService)
+        public ViewProcessingLearningModel(IBookingService bookingService)
         {
             _bookingService = bookingService;
         }
 
-        public IList<ProcessingTeachingRespone> ProcessingTeach { get; set; } = default!;
+        public IList<BookingTutorResponse> BookingTutor { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
 
             var allbookingList = _bookingService.GetAllBookingTutor();
             var bookingList = allbookingList.OrderByDescending(x => x.DateStart)
-                                            .Where(x => x.Status.Equals("Approved"))
-                                            .Select(x => new ProcessingTeachingRespone
+                                            .Where(x => x.Status.Equals("Pending"))
+                                            .Select(x => new BookingTutorResponse
                                             {
                                                 Id = x.Id,
                                                 StudentName = x.Student.Fullname,
-                                                StudentAddress = x.Student.Address,
-                                                Grade = x.Student.Grade,
-                                                Phone = x.Student.Phone,
                                                 ServiceName = x.Service.Service1,
                                                 DateStart = x.DateStart,
                                                 DateEnd = x.DateEnd
 
                                             });
-            ProcessingTeach = bookingList.ToList();
+            BookingTutor = bookingList.ToList();
         }
+
     }
 }
